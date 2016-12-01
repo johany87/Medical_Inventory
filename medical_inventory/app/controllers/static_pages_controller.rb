50e40edc444f46a3bb9a589fb8d1ced46353bf95
Johany Navarro <johany87@gmail.com>
2016-12-01 13:34:06 -0500

    add overdues products

--- medical_inventory/app/controllers/static_pages_controller.rb
+++ medical_inventory/app/controllers/static_pages_controller.rb
@@ -1,2 +1,21 @@
 class StaticPagesController < ApplicationController
+
+  def index
+    find_overdue_stocks
+    find_products_by_user
+    find_products_by_lab
+  end
+
+  def find_products_by_lab
+    @product_by_lab = Product.where("laboratory_id = '1'")
+  end
+
+  def find_products_by_user
+    @products_by_user = Stock.where("user_id = '1'")
+  end
+
+  def find_overdue_stocks
+    @overdue_stocks = Stock.where("due_date > ?", Date.today)
+  end
+
 end
