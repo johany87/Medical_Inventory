878a3f62355c3b2a8646d59b159b29d908dc2829
Johany Navarro <johany@gmail.com>
2016-12-01 21:49:32 -0500

    add filter complety

--- medical_inventory/app/controllers/filters_controller.rb
+++ medical_inventory/app/controllers/filters_controller.rb
@@ -2,17 +2,27 @@
 class FiltersController < ApplicationController
 
     def index
+      @products = Product.all
+      @usuarios = User.all
+      @laboratories = Laboratory.all
       search_products_by_lab
       search_products_by_user
+      search_products_by_price
     end #index
 
+
+    private
+
     def search_products_by_lab
-      lab = Laboratory.find_by(id: params[:laboratory_id])
-      @products_by_lab = Product.where("laboratory_id = ?", lab(id:))
+      @products_by_lab = Product.where(laboratory_id: params[:laboratory_id])
     end #search_products_by_lab
 
     def search_products_by_user
-      @products_by_user = Stock.where("user_id = '1'")
+      @products_by_user = Stock.where(user_id: params[:user_id])
     end #search_products_by_user
+
+    def search_products_by_price
+      @products_by_price = Product.where(price: params[:price])
+    end
 
 end #FiltersController
--- medical_inventory/config/routes.rb
+++ medical_inventory/config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+
   root to: "static_pages#index"
   devise_for :users, controllers:{
     sessions: 'users/sessions'}
@@ -6,6 +7,8 @@ Rails.application.routes.draw do
   resources :products
   resources :laboratories
   resources :stocks
-  resources :filters
+  scope :filters, controller: :filters do
+    get "/" =>  'filters#index', as: :filters
+  end
 
 end
